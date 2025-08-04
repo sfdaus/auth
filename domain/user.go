e443d5b1bba371623ea1e520da5d93336d83da7b
Zardarian Ahadika <zardarianahadika@gmail.com>
2025-08-04 23:44:59 +0700

    update user model

--- domain/user.go
+++ domain/user.go
@@ -7,14 +7,21 @@ import (
 
 // User ...
 type User struct {
-	ID        int64     `json:"id"`
-	Email     string    `json:"email"`
-	Password  string    `json:"password"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID           int64     `json:"id"`
+	Password     string    `json:"password"`
+	Username     string    `json:"username"`
+	PhoneNumber  string    `json:"phone_number"`
+	Email        string    `json:"email"`
+	TokenVersion string    `json:"token_version"`
+	IsActive     bool      `json:"is_active"`
+	CreatedAt    time.Time `json:"created_at"`
+	CreatedBy    string    `json:"created_by"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	UpdatedBy    string    `json:"updated_by"`
+	DeletedAt    time.Time `json:"deleted_at"`
 }
 
-// UserRepository represent the todos repository contract
+// UserRepository represent the users repository contract
 type UserRepository interface {
 	Create(ctx context.Context, user *User) error
 	GetByEmail(ctx context.Context, email string) (User, error)
